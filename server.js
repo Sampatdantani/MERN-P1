67c87268c853cf0903b78914cbf0236e15749abf
Sem dantani <137180561+Sampatdantani@users.noreply.github.com>
2025-01-04 16:24:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -36,10 +36,10 @@ app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-//rest api
-app.use("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index,html"));
-});
+// //rest api
+// app.use("*", function (req, res) {
+//   res.sendFile(path.join(__dirname, "./client/build/index,html"));
+// });
 
 //PORT
 const PORT = process.env.PORT || 8080;
